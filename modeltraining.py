9bda6e6a2f2cd9e746ca32a0ccc3499f80785d6b
ross.castroverde <ross.castroverde@overwatchid.com>
2019-06-20 15:58:46 +0800

    added racoon dataset

--- modeltraining.py
+++ modeltraining.py
@@ -2,5 +2,5 @@ from imageai.Prediction.Custom import ModelTraining
 
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
-model_trainer.setDataDirectory("/home/teravibe/ezeebuy/cartier")
+model_trainer.setDataDirectory("/home/teravibe/ezeebuy/racoon")
 model_trainer.trainModel(num_objects=3, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
