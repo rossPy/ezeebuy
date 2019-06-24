888baa4d4f02cc82e2fce4222f05129d38866c59
teravibe <teravibe@miner003.168.254.113>
2019-06-24 14:15:22 +0800

    modified modeltraining.py

--- modeltraining.py
+++ modeltraining.py
@@ -1,6 +1,11 @@
 from imageai.Prediction.Custom import ModelTraining
+import os
+
+my_path = os.getcwd()
 
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
-model_trainer.setDataDirectory("/home/teravibe/ezeebuy/racoon")
+
+model_trainer.setDataDirectory(os.path.join(my_path, "cartier"))
 model_trainer.trainModel(num_objects=3, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
+
